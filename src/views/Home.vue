398c4ec7773d3fbf88704d5231fe9cd1deffc293
Adem ilter <ademilter@gmail.com>
2020-08-01 17:37:00 +0300

    initial

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+
+  @media (--t) {
+    color:blue;
+  }
+}
+</style>
