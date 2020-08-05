6c9d6c557df20bdc04311c289bfa007771adafaa
Adem ilter <ademilter@gmail.com>
2020-08-06 00:14:14 +0300

    reset css ve variable css ayarlandı

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './styles/app.css'
 
 Vue.config.productionTip = false
 
