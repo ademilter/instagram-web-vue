e3c9b70f051c8ccd47e32cbc6019d91fe755f548
Adem ilter <ademilter@gmail.com>
2020-08-06 00:14:26 +0300

    tipografi için text componenti oluşturuldu

---
+++ src/components/CustomText.vue
@@ -0,0 +1,36 @@
+<template>
+  <component :is="tag" :class="size">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'CustomText',
+  props: {
+    tag: {
+      type: String,
+      default: 'span'
+    },
+    size: {
+      type: String,
+      default: 'normal', // normal:14, small:12, xsmall:11, xxsmall:10
+      validator: function(value) {
+        return ['normal', 'small', 'xsmall', 'xxsmall'].indexOf(value) !== -1
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.small {
+  font-size: 12px;
+}
+.xsmall {
+  font-size: 11px;
+}
+.xxsmall {
+  font-size: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <CustomText class="a1" tag="p">Merhaba dünya</CustomText>
+    <CustomText class="a2" tag="p" size="small">Merhaba dünya</CustomText>
+    <CustomText class="a3" tag="p" size="xsmall">Merhaba dünya</CustomText>
+    <CustomText tag="p" size="xxsmall">Merhaba dünya</CustomText>
   </div>
 </template>
 
 <script>
+import CustomText from '@/components/CustomText'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    CustomText
+  }
 }
 </script>
 
 <style scoped>
-h1 {
-  color: red;
-
-  @media (--t) {
-    color: blue;
-  }
+.a1 {
+  color: rgba(var(--ba8), 0.5);
+}
+.a2 {
+  color: rgb(var(--b3f));
+}
+.a3 {
+  color: rgb(var(--ba8));
 }
 </style>
