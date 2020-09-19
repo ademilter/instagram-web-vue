03a4d1403fe3827139571bcb63d2433ff30e14b9
halilcakar <hcakar.1992@gmail.com>
2020-09-19 04:37:41 +0300

    Videolarda için daha kolay görünüm için

--- src/router/index.js
+++ src/router/index.js
@@ -1,56 +1,30 @@
+import Home from '../views/home/index.vue'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/home/index.vue'
 
 Vue.use(VueRouter)
 
+const Direct = () => import(/* webpackChunkName: "profile" */ '../views/direct')
+const Explore = () => import(/* webpackChunkName: "profile" */ '../views/explore')
+const Profile = () => import(/* webpackChunkName: "profile" */ '../views/profile')
+const ProfilePost = () => import(/* webpackChunkName: "profile" */ '../views/profile/post')
+const ProfileIGTV = () => import(/* webpackChunkName: "profile" */ '../views/profile/igtv')
+const ProfileSave = () => import(/* webpackChunkName: "profile" */ '../views/profile/save')
+const ProfileTag = () => import(/* webpackChunkName: "profile" */ '../views/profile/tag')
+
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/direct',
-    name: 'Direct',
-    component: () => import(/* webpackChunkName: "profile" */ '../views/direct')
-  },
-  {
-    path: '/explore',
-    name: 'Explore',
-    component: () =>
-      import(/* webpackChunkName: "profile" */ '../views/explore')
-  },
+  { path: '/', name: 'Home', component: Home },
+  { path: '/direct', name: 'Direct', component: Direct },
+  { path: '/explore', name: 'Explore', component: Explore },
   {
     path: '/profile',
     name: 'Profile',
-    component: () =>
-      import(/* webpackChunkName: "profile" */ '../views/profile'),
+    component: Profile,
     children: [
-      {
-        path: '',
-        name: 'ProfilePost',
-        component: () =>
-          import(/* webpackChunkName: "profile" */ '../views/profile/post')
-      },
-      {
-        path: 'igtv',
-        name: 'ProfileIGTV',
-        component: () =>
-          import(/* webpackChunkName: "profile" */ '../views/profile/igtv')
-      },
-      {
-        path: 'save',
-        name: 'ProfileSave',
-        component: () =>
-          import(/* webpackChunkName: "profile" */ '../views/profile/save')
-      },
-      {
-        path: 'tag',
-        name: 'ProfileTag',
-        component: () =>
-          import(/* webpackChunkName: "profile" */ '../views/profile/tag')
-      }
+      { path: '', name: 'ProfilePost', component: ProfilePost },
+      { path: 'igtv', name: 'ProfileIGTV', component: ProfileIGTV },
+      { path: 'save', name: 'ProfileSave', component: ProfileSave },
+      { path: 'tag', name: 'ProfileTag', component: ProfileTag }
     ]
   }
 ]
